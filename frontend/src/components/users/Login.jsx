16051af4410e3bd3efc393863fbbeb7c1321dbf7
Abhigyan Tomar <104024722+Abhi0517@users.noreply.github.com>
2025-07-12 12:12:06 +0530

    Fix forgotPassword error

--- frontend/src/components/users/Login.jsx
+++ frontend/src/components/users/Login.jsx
@@ -67,7 +67,7 @@ const {isAuthenticated, loading, error} = useSelector(
                     onChange={(e) => setPassword(e.target.value)}
                   ></input>
                 </div>
-                <Link to="/users/forgetPassword" className="float-right mb-4">Forgot Password</Link>
+                <Link to="/users/forgotPassword" className="float-right mb-4">Forgot Password</Link>
                 <button
                   id="login_button"
                   type="submit"
